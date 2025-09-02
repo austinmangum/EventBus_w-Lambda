dc619f70e2053cf4fdc9481057049eecfa7ee79a
austinmangum <austin.mangum@yahoo.com>
2025-09-02 10:27:19 -0600

    removed read me

--- src/clients/avalara.ts
+++ src/clients/avalara.ts
@@ -6,6 +6,6 @@ export async function calcTax(lines: TaxLine[], shipTo: ShipTo) {
     const totalTax = +(subtotal * 0.07).toFixed(2);
     return { totalTax, lines: lines.map(l => ({ sku: l.sku, tax: +(l.qty * l.unitPrice * 0.07).toFixed(2) })) };
   }
-  // TODO: Avalara REST CreateTransaction impl
+  // TODO: Avalara REST CreateTransaction API call
   const e: any = new Error('Avalara not implemented'); e.name = 'FatalTaxError'; throw e;
 }
